97fcebd86e50b2e1ad8e5052d192cb80485fd294
okan-can-nazli <okancanazli@gmail.com>
2026-02-28 16:48:20 +0300

    add post detail page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import Login from "./Login";
 import Register from "./Register";
 import Dashboard from "./Dashboard";
 import CreatePost from "./CreatePost";
+import PostDetail from "./PostDetail";
 
 function App() {
   const path = window.location.pathname;
   if (path === "/register") return <Register />;
   if (path === "/dashboard") return <Dashboard />;
   if (path === "/create-post") return <CreatePost />;
+  if (path === "/post-detail") return <PostDetail />;
+
   return <Login />;
 }
 
